9da639dea3d3bbd2ede559c0263f97130bcdd2eb
Matt Auretta <mauretta2@gmail.com>
2018-11-24 18:44:06 -0700

    functional scraping into mongodb

---
+++ models/Article.js
@@ -0,0 +1,31 @@
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Create a new ArticleSchema object
+var ArticleSchema = new Schema({
+  // `title` is required and of type String
+  title: {
+    type: String,
+    required: true
+  },
+  // `link` is required and of type String
+  link: {
+    type: String,
+    required: true
+  },
+  // `comment` is an object that stores a Comment id
+  // The ref property links the ObjectId to the Comment model
+  // This allows us to populate the Article with an associated Comment
+  comment: {
+    type: Schema.Types.ObjectId,
+    ref: "Comment"
+  }
+});
+
+// This creates our model from the above schema, using mongoose's model method
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the Article model
+module.exports = Article;
--- server.js
+++ server.js
@@ -25,6 +25,48 @@ app.use(express.json());
 // Make public a static folder
 app.use(express.static("public"));
 
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/news-scraper", { useNewUrlParser: true });
+
+// Routes
+
+// A GET route for scraping the echoJS website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.newschoolers.com/home").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // Now, we grab every h2 within an article tag, and do the following:
+    $(".ns-card-title").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .children("a")
+        .text();
+      result.link = $(this)
+        .children("a")
+        .attr("href");
+        console.log("Result:")
+        console.log(result)
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, log it
+          console.log(err);
+        });
+    });
+
+    // Send a message to the client
+    res.json("Scrape Complete");
+  });
+});
+
 // Start the server
 app.listen(PORT, function() {
   console.log("App running on port " + PORT + "!");
