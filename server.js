043d857f4f522130594f4c16eb79e38f3e89674b
Matt Auretta <mauretta2@gmail.com>
2018-11-25 11:35:01 -0700

    linked handlebars

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var exphbs = require("express-handlebars");
 var logger = require("morgan")
 var mongoose = require("mongoose");
-
+var bodyParser = require("body-parser")
 //Scraping tools
 var axios = require("axios");
 var cheerio = require("cheerio");
@@ -15,32 +15,45 @@ var PORT = 3000;
 //Initialze express
 var app = express();
 
+// Use the express.static middleware to serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 //Configure middleware
 
 // Use morgan logger for logging requests
 app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/news-scraper", { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/news-scraper", {
+  useNewUrlParser: true
+});
 
 // Routes
 
-// A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
+// Serve index.handlebars to the root route.
+app.get("/", function(req, res) {
+    res.render("index");
+  });
+
+
+// A GET route for scraping the newschoolers website
+app.get("/scrape", function (req, res) {
   // First, we grab the body of the html with axios
-  axios.get("https://www.newschoolers.com/home").then(function(response) {
+  // Save an empty result object
+  var result = {};
+  axios.get("https://www.newschoolers.com/home").then(function (response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $(".ns-card-title").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
+    $(".ns-card-title").each(function (i, element) {
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
         .children("a")
@@ -48,26 +61,25 @@ app.get("/scrape", function(req, res) {
       result.link = $(this)
         .children("a")
         .attr("href");
-        console.log("Result:")
-        console.log(result)
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
-        .then(function(dbArticle) {
+        .then(function (dbArticle) {
           // View the added result in the console
           console.log(dbArticle);
         })
-        .catch(function(err) {
+        .catch(function (err) {
           // If an error occurred, log it
           console.log(err);
         });
     });
-
-    // Send a message to the client
-    res.json("Scrape Complete");
+  });
+  // Render handlebars
+  res.render("index", {
+    result: result
   });
 });
 
 // Start the server
-app.listen(PORT, function() {
+app.listen(PORT, function () {
   console.log("App running on port " + PORT + "!");
 });
